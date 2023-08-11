f5fc22b8edb372f04e7c8e0f14cbfd8c35a10a64
elanespereira <elanespereira@gmail.com>
2023-08-11 11:19:42 -0300

    Service layer, component registration

--- src/main/java/com/elanepereira/course/resources/UserResource.java
+++ src/main/java/com/elanepereira/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.elanepereira.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elanepereira.course.entities.User;
+import com.elanepereira.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users") //controlador rest que responde no caminho /user
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping //indica que responde a requisição do tipo get do http
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Elane","elanespereira@gmail.com","999999","12345654");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		//retorna a resposta de sucesso do http com a mensagem especificada no body
+	}
+	@GetMapping(value = "/{id}") //sempre que for uma requisicao tem de indicar o tipo, no caso GET
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok(obj);
 	}
 }
---
+++ src/main/java/com/elanepereira/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.elanepereira.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.elanepereira.course.entities.User;
+import com.elanepereira.course.repositories.UserRepository;
+
+@Service
+//@Component eh generico. O @service eh mais especifico para o caso
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
